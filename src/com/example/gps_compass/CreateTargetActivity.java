4e222a9a1463e76743bf010afa62003d123baf53
damenzi <damenzi@student.ethz.ch>
2013-10-23 22:39:35 +0200

    Layout 1 ready TODO: Getting GPS-data, storing with SharedPref

--- src/com/example/gps_compass/CreateTargetActivity.java
+++ src/com/example/gps_compass/CreateTargetActivity.java
@@ -1,17 +1,25 @@
 package com.example.gps_compass;
 
-import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
 
 public class CreateTargetActivity extends Activity {
 
+	
+	
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_create_target);
+		
+		RadioButton rb= (RadioButton) findViewById(R.id.radio_current_location);//RadioButton default set on
+		rb.setChecked(true);
 	}
 
 	@Override
@@ -21,15 +29,31 @@ public class CreateTargetActivity extends Activity {
 		return true;
 	}
 
-	
+	//Sourcecode from button_set
 public void setTarget(View view){
 		
 	
-	// save Target, set automatically as current Target
+		RadioButton rb= (RadioButton) findViewById(R.id.radio_current_location);
+		
+		DataInterface temp = new DataInterface();//contains a name and gps-coordinates
+	
+		EditText editText=(EditText) findViewById(R.id.set_target_name);// read out the target's name
+		temp.name = editText.getText().toString();	
+		
+		
+		if(rb.isChecked()){//Target: current location
+			
+			temp.coordinates=null;	//EXPAND: read in current coordinates	
+			
+		}
+		else{}//EXPAND: read in from EditText
+			
+		
+		// save Target (sharepref), set automatically as current Target
 		
 		Intent intent= new Intent(this, MainActivity.class);// goto Main
 		startActivity(intent);
-		
+	
 	}
 	
 }
---
+++ src/com/example/gps_compass/DataInterface.java
@@ -0,0 +1,12 @@
+package com.example.gps_compass;
+
+import android.location.LocationManager;
+
+
+public class DataInterface{
+	
+	public String name;
+	public LocationManager coordinates;
+	
+
+}
