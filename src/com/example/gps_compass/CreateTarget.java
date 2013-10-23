78d4a15694e6f770a8720cb818f900cc9d792b24
damenzi <damenzi@student.ethz.ch>
2013-10-23 14:24:36 +0200

    All necessary Windows created

--- src/com/example/gps_compass/CreateTarget.java
+++ src/com/example/gps_compass/CreateTarget.java
@@ -4,7 +4,7 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
-public class SelectTarget extends Activity {
+public class CreateTarget extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
