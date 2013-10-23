0e3edb82a3746824f2557046d3a62f4975c12a93
damenzi <damenzi@student.ethz.ch>
2013-10-23 14:16:10 +0200

    test

--- src/com/example/gps_compass/MainActivity.java
+++ src/com/example/gps_compass/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends Activity {
 		setContentView(R.layout.activity_main);
 	}
 	
-	//David ist doof
+	//Gian is awesome
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
