309e887c86573e4532abc3de9e209dcc44d88953
damenzi <damenzi@student.ethz.ch>
2013-10-23 21:37:49 +0200

    Select Target prepared

--- src/com/example/gps_compass/CreateTargetActivity.java
+++ src/com/example/gps_compass/CreateTargetActivity.java
@@ -2,21 +2,34 @@ package com.example.gps_compass;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
 
 public class CreateTargetActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_select_target);
+		setContentView(R.layout.activity_create_target);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.select_target, menu);
+		getMenuInflater().inflate(R.menu.create_target, menu);
 		return true;
 	}
 
+	
+public void setTarget(View view){
+		
+	
+	// save Target, set automatically as current Target
+		
+		Intent intent= new Intent(this, MainActivity.class);// goto Main
+		startActivity(intent);
+		
+	}
+	
 }
