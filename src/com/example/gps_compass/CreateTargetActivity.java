861edc58b459f7dc31e83197ac557f792e408871
damenzi <damenzi@student.ethz.ch>
2013-10-27 12:44:14 +0100

    GPS implemented

--- src/com/example/gps_compass/CreateTargetActivity.java
+++ src/com/example/gps_compass/CreateTargetActivity.java
@@ -2,58 +2,105 @@ package com.example.gps_compass;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.EditText;
-import android.widget.RadioButton;
 
-public class CreateTargetActivity extends Activity {
+public class CreateTargetActivity extends Activity implements LocationListener {
 
 	
-	
+	boolean checked=false;
+	LocationManager manager;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_create_target);
 		
-		RadioButton rb= (RadioButton) findViewById(R.id.radio_current_location);//RadioButton default set on
-		rb.setChecked(true);
+		
+		manager = (LocationManager) this.getSystemService(LOCATION_SERVICE); 
+		manager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 400, 1, this);//get Location	
+		
+		CheckBox cl = (CheckBox)findViewById(R.id.checkbox_use_current_location);//RadioButton default set on
+		cl.setChecked(true);
 	}
 
+	
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.create_target, menu);
 		return true;
 	}
-
-	//Sourcecode from button_set
-public void setTarget(View view){
-		
 	
-		RadioButton rb= (RadioButton) findViewById(R.id.radio_current_location);
+	
+	public void onCheckBoxClicked (View view){
+		//is the view now checked?
+		checked = ((CheckBox) view).isChecked();
+	}
+
+	
+	//Sourcecode from button_set
+	public void setTarget(View view){		
+	
+//		RadioButton rb= (RadioButton) findViewById(R.id.radio_current_location);
 		
 		DataInterface temp = new DataInterface();//contains a name and gps-coordinates
 	
 		EditText editText=(EditText) findViewById(R.id.set_target_name);// read out the target's name
 		temp.name = editText.getText().toString();	
 		
-		
-		if(rb.isChecked()){//Target: current location
+		if(checked){//Target: current location
 			
-			temp.coordinates=null;	//EXPAND: read in current coordinates	
+			//Check: GPS enabled!
+			temp.coordinates=manager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
 			
 		}
-		else{}//EXPAND: read in from EditText
+		else{//EXPAND: read in from EditText			
 			
-		
+		}
+			
+				
 		// save Target (sharepref), set automatically as current Target
 		
 		Intent intent= new Intent(this, MainActivity.class);// goto Main
 		startActivity(intent);
+	}
 	
+	
+	
+
+
+	
+	
+	@Override
+	public void onLocationChanged(Location arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onProviderDisabled(String provider) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onProviderEnabled(String provider) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onStatusChanged(String provider, int status, Bundle extras) {
+		// TODO Auto-generated method stub
+		
 	}
 	
 }
--- src/com/example/gps_compass/DataInterface.java
+++ src/com/example/gps_compass/DataInterface.java
@@ -1,12 +1,12 @@
 package com.example.gps_compass;
 
-import android.location.LocationManager;
+import android.location.Location;
 
 
 public class DataInterface{
 	
 	public String name;
-	public LocationManager coordinates;
+	public Location coordinates;
 	
 
 }
